ec27c2716a01171b2c514de68b8b1d86b727c783
Leemengwei <1099366685@qq.com>
2020-01-01 16:15:01 +0800

    Update C_load_and_run_API.cpp
    preproccess and tensor2vector<float>

--- src/C_project_windows/C_load_and_run_API.cpp
+++ src/C_project_windows/C_load_and_run_API.cpp
@@ -1,3 +1,5 @@
+#define num_of_cols 25
+#define num_of_rows 1
 
 #include <iostream>
 #include <memory>
@@ -26,9 +28,9 @@ torch::jit::script::Module get_model(const char* model_path, bool use_cuda) {
 }
 
 //Function to convert data
-std::vector<torch::jit::IValue> convert_data(bool use_cuda, int flat[][25]) {
+std::vector<torch::jit::IValue> convert_data(bool use_cuda, float flat[][num_of_cols]) {
 
-  at::Tensor data = torch::from_blob(flat, {6,25}, torch::kInt);
+  at::Tensor data = torch::from_blob(flat, {num_of_rows,num_of_cols}, torch::kFloat);
   data = data.toType(torch::kFloat);
   if (use_cuda){
     data=data.to(at::kCUDA);
@@ -39,7 +41,7 @@ std::vector<torch::jit::IValue> convert_data(bool use_cuda, int flat[][25]) {
 }
 
 //Function to run
-at::Tensor predict(bool use_cuda, torch::jit::script::Module model, int flat[][25]) {
+at::Tensor predict(bool use_cuda, torch::jit::script::Module model, float flat[][num_of_cols]) {
   //Convert
   std::vector<torch::jit::IValue> inputs = convert_data(use_cuda, flat);
   //and run
@@ -48,10 +50,26 @@ at::Tensor predict(bool use_cuda, torch::jit::script::Module model, int flat[][2
   return output;
 }
 
+//Function to preprocess input data
+void preprocess_data(float flat[][num_of_cols]) {
+    float mean[num_of_rows][num_of_cols] = {
+        2.51065213e-04, 2.26194300e+02, -3.87426864e-03, -3.87426074e-03, 2.22841566e-04, 3.23421159e+01, -3.59547080e-01, -3.30278722e-03, -6.44383032e-04, 8.86972255e+01, 8.88570940e-02, 2.07215009e-03, -5.10639766e-02, -1.07646179e+00, -5.16060366e-03, -1.96551706e-05, -3.95824362e-04, -4.44822192e-01, 1.01560208e-03, -7.17683819e-05, 1.0, 8.42130893e-06, 1.0, 1.0, 1.0,
+    };
+    float std[num_of_rows][num_of_cols] = {
+        1.49259451e-02, 3.22330006e-03, 1.96332955e-02, 1.96332663e-02, 8.86414915e-03, 1.26311744e+02, 1.40872237e+00, 1.98642367e-02, 2.82137393e-02, 1.65190416e+02, 3.03534892e-01, 1.14498188e-02, 2.04909340e+02, 1.20522046e+02, 5.97290116e-02, 4.98756114e-02, 1.37543738e-01, 1.08568109e+02, 1.89032465e-02, 6.36430634e-04, 1.0, 1.48271675e-04, 1.0, 1.0, 1.0,
+    };
+    //flat=((flat-mean)/std);
+    for (int i = 0; i < num_of_rows; i++) {
+        for (int j = 0; j < num_of_cols; j++) {
+            flat[i][j] = (flat[i][j] - mean[i][j]) / std[i][j];
+        }
+    }
+}
+
 
 
 int main(int argc, const char* argv[]) {
-
+ 
   //Config:
   bool use_cuda=(argc>1)?true:false;
   if (use_cuda){std::cout<<"USING GPU"<<std::endl;}
@@ -61,24 +79,23 @@ int main(int argc, const char* argv[]) {
   const char* model_path="../models/NN_weights_uniform_C.pt";
   torch::jit::script::Module model=get_model(model_path, use_cuda);
 
-  while (true)
-  {
+ // while (true){
       //Get data:
-      int flat[6][25] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+      float flat[num_of_rows][num_of_cols]={
+          1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
       };
-      //Forward run loop:
+
+      //Preprocess data:
+      preprocess_data(flat);
+
+      //Forward run:
       at::Tensor output;
       output = predict(use_cuda, model, flat);
-      //Report:
-      //std::cout << flat << std::endl;
-      //std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/99) << '\n';
-  }
+
+      //Convert tensor back to float, using vector:
+      std::vector<float> out(output.data<float>(), output.data<float>() + output.numel());
+      std::cout << out << '\n';
+      
+ // }
 }
-
-
 
