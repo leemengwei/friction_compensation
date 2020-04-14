0ebb038d3f2e6d3382bbf84e95691e458f30e86e
leemengwei <1099366685@qq.com>
2020-04-14 17:43:18 +0800

    up

--- src/NN_compose.py
+++ src/NN_compose.py
@@ -188,36 +188,40 @@ if __name__ == "__main__":
     #And show:
     axes = []
     #Visualize:
-    plt.ion()
-    if args.VISUALIZATION:
-        fig = plt.figure(figsize=(16,9))
-        for temp_axis in range(1,7):
-            axes.append(fig.add_subplot(2,3,temp_axis))
-            args.axis_num = temp_axis
-            local_axis_num = args.axis_num - 1
-            axes[temp_axis-1].plot(meassured_dict[temp_axis], label=r'real_target', alpha=0.5)
-            axes[temp_axis-1].scatter(range(len(raw_plans_dict[temp_axis])), planned_dict[temp_axis], label=r'dynamic_model+gravity', color='gray', s=1, alpha=0.5)
-            axes[temp_axis-1].scatter(part1_index_dict[temp_axis], compensated_dict[temp_axis][part1_index_dict[temp_axis]], label=r'after compensate', color='green', s=1)
-            axes[temp_axis-1].scatter(part2_index_dict[temp_axis], compensated_dict[temp_axis][part2_index_dict[temp_axis]], label=r'after compensate', color='red', s=1)
-            axes[temp_axis-1].legend()
-            axes[temp_axis-1].set_title("Axis:{2:d}, Error treated: {0:.2f}%, original:{1:.2f}%".format(error_treated_dict[temp_axis], error_original_dict[temp_axis], int(temp_axis)))
-            #print("One of the reason it may shows higher error rate than expected is because some uniform speed range be ommited during train/val evaluation")
+    #plt.ion()
+    fig = plt.figure(figsize=(16,9))
+    for temp_axis in range(1,7):
+        axes.append(fig.add_subplot(2,3,temp_axis))
+        args.axis_num = temp_axis
+        local_axis_num = args.axis_num - 1
+        axes[temp_axis-1].plot(range(len(meassured_dict[temp_axis])), meassured_dict[temp_axis], label=r'real_target', alpha=0.5)
+        axes[temp_axis-1].scatter(range(len(raw_plans_dict[temp_axis])), planned_dict[temp_axis], label=r'dynamic_model+gravity', color='gray', s=1, alpha=0.5)
+        axes[temp_axis-1].scatter(part1_index_dict[temp_axis], compensated_dict[temp_axis][part1_index_dict[temp_axis]], label=r'after compensate', color='green', s=1)
+        axes[temp_axis-1].scatter(part2_index_dict[temp_axis], compensated_dict[temp_axis][part2_index_dict[temp_axis]], label=r'after compensate', color='red', s=1)
+        axes[temp_axis-1].legend()
+        axes[temp_axis-1].set_title("Axis:{2:d}, Error treated: {0:.2f}%, original:{1:.2f}%".format(error_treated_dict[temp_axis], error_original_dict[temp_axis], int(temp_axis)))
+        #print("One of the reason it may shows higher error rate than expected is because some uniform speed range be ommited during train/val evaluation")
         fig.suptitle("Path name: %s"%args.data_path.split('/')[-1])
-        plt.savefig("../pngs/%s"%args.data_path.split('/')[-1].replace('prb-log','png'))
+    plt.savefig("../pngs/%s"%args.data_path.split('/')[-1].replace('prb-log','png'), dpi=500)
+    if args.VISUALIZATION:
         plt.show()
+    plt.close()
 
-        #Response surface:
-        axes = []
-        fig = plt.figure(figsize=(16,9))
-        from mpl_toolkits.mplot3d import Axes3D
-        for temp_axis in range(1,7):
-            axes.append(fig.add_subplot(2,3,temp_axis,projection='3d'))
-            args.axis_num = temp_axis
-            if args.VISUALIZATION:
-                plot_utils.response_surface(axes[temp_axis-1], models_dict[temp_axis], inputs_dict[temp_axis])
-            np.savetxt("../output/NN_compensation_%s.txt"%temp_axis, compensate_full_series_dict[temp_axis])
-        plt.savefig("../pngs/response_surf.png")
+    #Response surface:
+    axes = []
+    fig = plt.figure(figsize=(16,9))
+    from mpl_toolkits.mplot3d import Axes3D
+    for temp_axis in range(1,7):
+        axes.append(fig.add_subplot(2,3,temp_axis,projection='3d'))
+        args.axis_num = temp_axis
+        plot_utils.response_surface(axes[temp_axis-1], models_dict[temp_axis], inputs_dict[temp_axis])
+        np.savetxt("../output/NN_compensation_%s.txt"%temp_axis, compensate_full_series_dict[temp_axis])
+    plt.savefig("../pngs/response_surf.png",dpi=500)
+    if args.VISUALIZATION:
         plt.show()
+    plt.close()
+
+    print("Original: %s,\n Treated: %s"%(error_original_dict, error_treated_dict))
     print("Done")
 
 
--- src/all_axis_compose.sh
+++ src/all_axis_compose.sh
@@ -1,6 +1,14 @@
 # NN_compose itself works with all six axis, this script is to iterate through paths.
-python NN_compose.py -V --data_path=../data/standard_path/realtime-20200326-162818.rec-data-maingene100%.prb-log
-python NN_compose.py -V --data_path=../data/standard_path/realtime-20200326-163224.rec-data-maingene50%.prb-log
-python NN_compose.py -V --data_path=../data/standard_path/realtime-20200326-165204.rec-data-maingene20%.prb-log
-python NN_compose.py -V --data_path=../data/standard_path/realtime-20200326-171242.rec-data-rrr.prb-log
-python NN_compose.py -V --data_path=../data/standard_path/realtime-20200326-171651.rec-data-testzhixian.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200326-162818.rec-data-maingene100%.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200326-163224.rec-data-maingene50%.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200326-165204.rec-data-maingene20%.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200326-171242.rec-data-rrr.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200326-171651.rec-data-testzhixian.prb-log
+
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-150531-jiepai-low.rec-data.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-150833-jiepai-high.rec-data.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-151740-bi-low.rec-data.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-152042-bi-high.rec-data.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-163642-welding_low.rec-data.prb-log
+python NN_compose.py --data_path=../data/standard_path/realtime-20200413-163945-welding-high.rec-data.prb-log
+
